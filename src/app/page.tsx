7dc55d44f141db4088cb8d804c4bcb89048eacd7
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-10-26 18:01:42 +0530

    Refactor database connection and update Home component to fetch users

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,19 @@
 import { Button } from "@/components/ui/button";
+import { client, db } from "@/db";
+import { usersTable } from "@/db/schema";
 
 
-export default function Home() {
-  return (
-   <div className="bg-teal-300 h-40 w-full">
-    <Button variant={'ghost'}>Hello</Button>
-   </div>
-  );
+export default async function Home() {
+  const users = await db.select().from(usersTable);
+  console.log('Getting all users from the database: ', users)
+  
+
+return (
+  <div className="bg-teal-300 h-40 w-full">
+    <Button variant="ghost">
+    Hi
+    </Button>
+     
+  </div>
+);
 }
